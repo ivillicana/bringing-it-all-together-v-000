88b1781d9fff75c89c9fe87e4e3b7479e5ad69e7
Isaac Villicana <villicana.i@gmail.com>
2018-06-05 14:43:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,8 +4,6 @@ class Dog
 
   def initialize (attributes, id=nil)
     attributes.each {|key, value| self.send("#{key}=", value)}
-    @id = id
-    @name = name
-    @breed = breed
+    
   end
 end
