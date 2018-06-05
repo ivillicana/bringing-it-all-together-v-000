47461664b5738f35d7f58d34db422625f62111dd
Isaac Villicana <villicana.i@gmail.com>
2018-06-05 15:05:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,6 +27,17 @@ class Dog
     self.new(id: row[0], name: row[1], breed: row[2])
   end
 
+  def self.find_by_name (name)
+    sql = <<-SQL
+      SELECT *
+        FROM dogs
+        WHERE name = ?
+        LIMIT 1
+        SQL
+    found_dog = DB[:conn].execute(sql, name).flatten
+    self.new_from_db(found_dog)
+  end
+
   def save
     if self.id
       self.update
